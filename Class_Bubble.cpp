8c79a970a28d88166c1b40ca865c8ef20f5bd5d9
Sofía Pinto <118233041+sofiapintogalvez@users.noreply.github.com>
2024-08-31 10:57:01 -0500

    Add files via upload

---
+++ Class_Bubble.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+struct ASC
+{
+	bool operator()(T a, T b)
+	{
+		return a > b;
+	}
+};
+
+template <class T>
+struct DESC
+{
+	bool operator()(T a, T b)
+	{
+		return a < b;
+	}
+};
+
+template <class T, class O>
+struct bubble
+{
+	void operator()(T* ini, T* fin)
+	{
+		O op;
+		bool sigue = true;
+		while (ini < fin && sigue)
+		{
+			sigue = false;
+			for (int* p = ini; p < fin; p++)
+			{
+				if (op(*p, *(p + 1)))
+				{
+					swap(*p, *(p + 1));
+					sigue = true;
+				}
+			}
+			fin--;
+		}
+	}
+};
+
+int main()
+{
+	bubble<int, ASC<int>> b;
+
+	int A[6] = { 6,5,4,3,2,1 };
+
+	b(A, A + 5);
+
+	for (int i = 0; i < 6; i++)
+		cout << A[i] << " ";
+}
