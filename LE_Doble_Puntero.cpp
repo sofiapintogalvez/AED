fd9e84b59b778769634e8948cb9961a8593c09a8
Sofía Pinto <118233041+sofiapintogalvez@users.noreply.github.com>
2024-10-11 10:17:22 -0500

    Add files via upload

---
+++ LE_Doble_Puntero.cpp
@@ -0,0 +1,86 @@
+// Lista enlazada con doble puntero
+
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+    int v;
+    Node* next;
+    Node(int x)
+    {
+        v = x;
+        next = NULL;
+    }
+};
+
+struct LE
+{
+    Node* head = NULL;
+    bool find(int x, Node**& p);
+    bool ins(int x);
+    bool rem(int x);
+    void print();
+    ~LE();
+};
+
+bool LE::find(int x, Node**& p)
+{
+    p = &head;
+    while (*p && (*p)->v < x)
+        p = (&(*p)->next);
+
+    return *p && (*p)->v == x;
+}
+
+bool LE::ins(int x)
+{
+    Node** p;
+    if (find(x, p)) return 0;
+    Node* t = new Node(x);
+    t->next = *p;
+    *p = t;
+    return 1;
+}
+
+bool LE::rem(int x)
+{
+    Node** p;
+    if (!find(x, p)) return 0;
+    Node* t = *p;
+    *p = t->next;
+    delete t;
+    return 1;
+}
+
+void LE::print()
+{
+    cout << "head->";
+    for (Node* p = head; p; p = p->next)
+        cout << p->v << "->";
+    cout << "null\n";
+}
+
+LE::~LE()
+{
+    Node* tmp;
+    while (head)
+    {
+        tmp = head;
+        head = head->next;
+        delete tmp;
+    }
+}
+
+int main()
+{
+    LE lista;
+
+    lista.ins(7);
+    lista.ins(4);
+    lista.ins(14);
+    lista.ins(11);
+
+    lista.print();
+}
